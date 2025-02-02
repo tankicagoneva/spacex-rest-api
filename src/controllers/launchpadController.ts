c386d8386b4dbf5f84692e5d00a737fc0d7e21b2
tankicagoneva <tankicaspasev@gmail.com>
2025-02-02 20:34:59 +0000

    docs: add JSDoc comments for launchpad controller and service methods

--- src/controllers/launchpadController.ts
+++ src/controllers/launchpadController.ts
@@ -1,6 +1,19 @@
 import { RequestHandler } from "express";
 import * as launchpadService from "../services/launchpadService.js";
 
+
+
+/**
+ * Retrieves all launchpads and sends them in the response.
+ * 
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - The next middleware function.
+ * 
+ * @returns A JSON response with the list of all launchpads.
+ * 
+ * @throws Passes any errors to the next function.
+ */
 export const getAllLaunchpads: RequestHandler = async (req, res, next) => {
   try {
     const launchpads = await launchpadService.getAllLaunchpads();
@@ -10,6 +23,18 @@ export const getAllLaunchpads: RequestHandler = async (req, res, next) => {
   }
 };
 
+
+/**
+ * Handles the request to get a launchpad by its ID.
+ * 
+ * @param req - The request object, containing the launchpad ID in the parameters.
+ * @param res - The response object, used to send back the launchpad data or an error message.
+ * @param next - The next middleware function in the stack.
+ * 
+ * @returns A JSON response with the launchpad data if found, or a 404 error message if not found.
+ * 
+ * @throws Will pass any errors to the next function.
+ */
 export const getLaunchpadById: RequestHandler = async (req, res, next) => {
   try {
     const launchpad = await launchpadService.getLaunchpadById(req.params.id);
--- src/services/launchpadService.ts
+++ src/services/launchpadService.ts
@@ -1,6 +1,10 @@
 import db from "../db/connection.js";
 
-// Get all launchpads
+/**
+ * Retrieves all launchpads from the database.
+ *
+ * @returns {Promise<any[]>} A promise that resolves to an array of launchpad objects.
+ */
 export const getAllLaunchpads = async (): Promise<any[]> => {
   const launchpads = await db.query(
     "SELECT * FROM public.launchpads ORDER BY id ASC",
@@ -9,7 +13,12 @@ export const getAllLaunchpads = async (): Promise<any[]> => {
   return launchpads.rows;
 };
 
-// Get a launchpad by id
+/**
+ * Retrieves a launchpad by ID from the database.
+ *
+ * @param {string} id - The ID of the launchpad to retrieve.
+ * @returns {Promise<any>} A promise that resolves to the launchpad data.
+ */
 export const getLaunchpadById = async (id: string): Promise<any> => {
   const launchpad = await db.query(
     "SELECT * FROM public.launchpads WHERE id = $1",
