8e1f31bf4e4a31781bab30bd11b84898c164b7da
tankicagoneva <tankicaspasev@gmail.com>
2025-02-04 19:50:53 +0000

    feat: implement CRUD operations

--- src/services/launchpadService.ts
+++ src/services/launchpadService.ts
@@ -1,28 +1,72 @@
-import db from "../db/connection.js";
+import prisma from "../db/client.js";
 
 /**
  * Retrieves all launchpads from the database.
  *
- * @returns {Promise<any[]>} A promise that resolves to an array of launchpad objects.
+ * @returns A list of all launchpads.
  */
-export const getAllLaunchpads = async (): Promise<any[]> => {
-  const launchpads = await db.query(
-    "SELECT * FROM public.launchpads ORDER BY id ASC",
-    [],
-  );
-  return launchpads.rows;
+export const getAllLaunchpads = async () => {
+  return prisma.launchpads.findMany({
+    orderBy: {
+      id: "asc",
+    },
+  });
+};
+
+ 
+/**
+ * Retrieves a launchpad by its ID.
+ *
+ * @param id - The ID of the launchpad to retrieve.
+ * @returns The launchpad with the specified ID.
+ */
+export const getLaunchpadById = async (id: string) => {
+  return prisma.launchpads.findUnique({
+    where: {
+      id: id,
+    },
+  });
 };
 
 /**
- * Retrieves a launchpad by ID from the database.
+ * Creates a new launchpad.
  *
- * @param {string} id - The ID of the launchpad to retrieve.
- * @returns {Promise<any>} A promise that resolves to the launchpad data.
+ * @param launchpad - The launchpad to create.
+ * @returns The new created launchpad.
  */
-export const getLaunchpadById = async (id: string): Promise<any> => {
-  const launchpad = await db.query(
-    "SELECT * FROM public.launchpads WHERE id = $1",
-    [id],
-  );
-  return launchpad.rows[0];
+export const createLaunchpad = async (launchpad: any) => {
+  return prisma.launchpads.create({
+    data: launchpad,
+  });
 };
+
+/**
+ * Updates a launchpad by its ID.
+ *
+ * @param id - The ID of the launchpad to update.
+ * @param launchpad - The updated launchpad data.
+ * @returns The updated launchpad.
+ */
+export const updateLaunchpad = async (id: string, launchpad: any) => {
+  return prisma.launchpads.update({
+    where: {
+      id: id,
+    },
+    data: launchpad,
+  });
+};
+
+/**
+ * Deletes a launchpad by its ID.
+ *
+ * @param id - The ID of the launchpad to delete.
+ * @returns The deleted launchpad.
+ */
+export const deleteLaunchpad = async (id: string) => {
+  return prisma.launchpads.delete({
+    where: {
+      id: id,
+    },
+  });
+};
+
