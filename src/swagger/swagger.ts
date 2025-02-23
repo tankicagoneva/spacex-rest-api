c71c00082a94d129e4686e36bd5141e53c516cd7
tankicagoneva <tankicaspasev@gmail.com>
2025-02-23 21:09:13 +0000

    fix: improve documentation

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -1,29 +1,10 @@
-import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import yaml from "js-yaml";
+import fs from "fs";
+import path from "path";
 
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "SpaceX Launchpads API",
-      version: "1.0.0",
-      description: "A REST API for SpaceX Launchpads",
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-        description: "Development server",
-      },
-    ],
-  },
-  apis: [
-    "./src/docs/routes/*.yaml",
-    "./src/docs/schemas/*.yaml",
-    "./src/routes/*.ts",
-  ],
-};
-
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
+const swaggerFilePath = path.join(process.cwd(), "src", "docs", "api-doc.yaml"); 
+const swaggerSpec = yaml.load(fs.readFileSync(swaggerFilePath, "utf8")) as Record<string, any>;
 
 export const swaggerDocs = swaggerUi.setup(swaggerSpec, {
   swaggerOptions: {
