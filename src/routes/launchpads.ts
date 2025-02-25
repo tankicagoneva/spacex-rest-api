1d9c6a7c61c1c011602cc0a53f6f76b73e834c44
tankicagoneva <tankicaspasev@gmail.com>
2025-02-25 22:09:59 +0000

    feat: add validation and middleware

--- src/routes/launchpads.ts
+++ src/routes/launchpads.ts
@@ -7,11 +7,14 @@ import {
   updateLaunchpad,
   deleteLaunchpad,
 } from "../controllers/launchpadController.ts";
+import { validateRequest } from "../middleware/validateRequest.ts";
+import { createLaunchpadsSchema, updateLaunchpadsSchema, getLaunchpadsByIdSchema } from "../validators/launchpadsValidators.ts";
 
 export const router = express.Router();
 
 router.get("/", getAllLaunchpads);
-router.get("/:id", getLaunchpadById);
-router.post("/", createLaunchpad);
-router.put("/:id", updateLaunchpad);
+router.get("/:id", validateRequest(getLaunchpadsByIdSchema), getLaunchpadById);
+
+router.post("/", validateRequest(createLaunchpadsSchema), createLaunchpad);
+router.put("/:id", validateRequest(updateLaunchpadsSchema), updateLaunchpad);
 router.delete("/:id", deleteLaunchpad);
