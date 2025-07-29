c5f7908d2ba7374f32698ba7d36e13a6ac3f98cd
Your <your_email@example.com>
2025-07-29 21:38:16 +0100

    feat: add Clerk authentication and cookie parser middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,10 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
 import dotenv from "dotenv"
 import { router as launchpadRouter } from "../routes/launchpads.js"
 import { swaggerServe, swaggerDocs } from "../swagger/swagger.ts"
 import serverless from "serverless-http"
+import cookieParser from 'cookie-parser';
+import { clerkMiddleware, getAuth, requireAuth } from '@clerk/express'
 
 
 dotenv.config()
@@ -12,6 +14,8 @@ const PORT = process.env.PORT || 3000
 
 
 // middleware
+app.use(cookieParser());
+app.use(clerkMiddleware())
 app.use(express.json())
 
 app.use("/api/docs", swaggerServe, swaggerDocs)
